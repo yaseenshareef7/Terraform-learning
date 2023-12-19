3134946eca49eb5bbe70d8c55b75cc072a7ae10c
yaseenshareef7 <152978872+yaseenshareef7@users.noreply.github.com>
2023-12-19 19:59:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 # Customer managed KMS key
 ###########################
 resource "aws_s3_bucket" "example" {
-  bucket = "buchddahghfffdat30009ddadada78738938967576775553"
+  bucket = "buchddafhghfffdat30009ddadada78738938967576775553"
 
   tags = {
     Name        = "bucket00097873893893"
